283f4d260cc193f9ec0d59371a8c87857b201ff6
Martin <martin_nguyen@dell.com>
2020-07-15 11:25:53 -0700

    linux updates

--- linux/docker_utils.py
+++ linux/docker_utils.py
@@ -14,6 +14,7 @@ class docker_utility:
 		else:
 			print ("Docker is not installed. Starting Docker installation.")
 
+<<<<<<< HEAD
 			# Remove docker files
 			subprocess.run(['sudo', 'apt-get', 'remove', 'docker', 'docker-engine', 'docker.io', 'containerd', 'runc'])
 
@@ -29,6 +30,23 @@ class docker_utility:
 
 			# Set Docker Stable Repository
 			subprocess.run(['sudo', 'add-apt-repository', "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"])
+=======
+			# Install Docker required packages
+			subprocess.run(['sudo', 'apt-get', 'clean'])
+			subprocess.run(['sudo', 'apt-get', 'update'])
+			subprocess.run(['sudo', 'apt-get', 'install', '-y', 'apt-transport-https', 'ca-certificates', 'curl', 'gnupg-agent', 'software-properties-common'])
+
+			# Add Docker's GPG Key
+			curl = subprocess.Popen(["curl", "-fsSL", "https://download.docker.com/linux/ubuntu/gpg"], stdout=subprocess.PIPE)
+			aptkey = subprocess.Popen(["sudo", "apt-key", "add"], stdin=curl.stdout, stdout=subprocess.PIPE)
+			curl = aptkey.communicate()[0]
+
+			# Remove docker files
+			subprocess.run(['sudo', 'apt-get', 'remove', 'docker', 'docker-engine', 'docker.io', 'containerd', 'runc'])
+
+			# Set Docker Stable Repository
+			subprocess.run(['sudo', 'add-apt-repository', "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"])
+>>>>>>> db7a9d4cfaed23506671a1e743cd02e35143056e
 
 			# Install Docker CE
 			subprocess.run(['sudo', 'apt-get', 'update'])
@@ -45,4 +63,8 @@ class docker_utility:
 			# Start Docker Service
 			subprocess.run(['sudo', 'systemctl', 'start', 'docker'])
 
+<<<<<<< HEAD
 			print ("Docker installation complete.")
+=======
+			print ("Docker installation complete.")
+>>>>>>> db7a9d4cfaed23506671a1e743cd02e35143056e
